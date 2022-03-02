be2da048529d019b244d2ab1f4ccc3c98e4b5fb9
UmiNoKaibutsu <Hassangh01001000@gmail.com>
2022-03-02 07:39:17 +0300

    first commit

---
+++ myProject01DoubleFiexed/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myProject01DoubleFiexed.Models;
+
+namespace myProject01DoubleFiexed.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
